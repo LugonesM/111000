69d3dbb5be51f36a6db0173d760bf92166213b55
LugonesM <54716002+LugonesM@users.noreply.github.com>
2020-10-29 23:02:40 -0300

    Update Heroe.java

--- moduloJava/supers/src/main/java/com/mvl/supers/Heroe.java
+++ moduloJava/supers/src/main/java/com/mvl/supers/Heroe.java
@@ -32,20 +32,30 @@ public class Heroe extends Supers_x {
    
    public int Controla_oxipital(){
     int controla_oxipital = 4;
+    if(this.indiceBondad>4){ 
+        controla_oxipital = 6;
+        System.out.print("---El heroe ataca con Control oxipital, resta 6---");
+    }
+    else{
+    System.out.print("---El heroe ataca con Control oxipital, resta 4---");
+    }
     return controla_oxipital;
 }
    public int Controla_cerebelo(){
        int cont_cerebel=3;
+       System.out.print("---El heroe ataca con Control cerebelo, resta 3---");
        return cont_cerebel;
    }
    
    public int Controla_lobulo_temporal(){
        int lobulo_temporal = 4;
+       System.out.print("---El heroe ataca con Control lobulo temporal, resta 4---");
        return lobulo_temporal;
    }
    
    public int Lobulo_frontal(){
        int lobulo_frontal = 5;
+       System.out.print("---El heroe ataca con Lobulo frontal, resta 5---");
        return lobulo_frontal;
    }
     
