727fb6b2586c09273b5a6c993411f6688d0ee699
LugonesM <54716002+LugonesM@users.noreply.github.com>
2020-10-29 23:00:40 -0300

    Update Malvado.java

--- moduloJava/supers/src/main/java/com/mvl/supers/Malvado.java
+++ moduloJava/supers/src/main/java/com/mvl/supers/Malvado.java
@@ -3,12 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mvl.supers;
-
-/**
- *
- * @author mel
- */
 public class Malvado extends Supers_x{
     private int indiceMaldad;
 
@@ -33,22 +27,32 @@ public class Malvado extends Supers_x{
     
    public int Rayo_violeta(){
        int rayo_violeta = 3;
+       System.out.print("---El villano ataca con Rayo violeta, resta 3---");
        return rayo_violeta;
    }
   
    
    public int Rayo_verde(){
        int rayo_verde = 2;
+       System.out.print("---El villano ataca con Rayo verde, resta 2---");
        return rayo_verde;
    }
    
    public int Rayo_azul(){
        int rayo_azul = 4;
+       System.out.print("---El villano ataca con Rayo azul, resta 4---");
        return rayo_azul;
    }
    
    public int Rayo_rojo(){
        int rayo_rojo = 1;
+       if(this.indiceMaldad>4){
+           rayo_rojo=10;
+           System.out.print("---El villano ataca con Rayo rojo, resta 10---");
+       }
+       else{
+       System.out.print("---El villano ataca con Rayo rojo, resta 1---");
+       }
        return rayo_rojo;
    }
    
